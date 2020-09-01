b9a12d9a188f9d6120a124093b645829deedf71e
Luke Chen <luke.chen.2010@gmail.com>
2020-09-01 01:30:35 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -10,17 +10,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+
       </div>
     )
   }
 
-  componentDidMount() {
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => response.json())
-      .then(({people}) => this.setState({ peopleInSpace: people }))
-
-  }
 
 }
 
