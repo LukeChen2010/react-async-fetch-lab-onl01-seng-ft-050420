11217581e25c293bbc4eeef1ddc6aaf4bb37d950
Luke Chen <luke.chen.2010@gmail.com>
2020-09-01 01:32:10 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ peopleInSpace: people }))
+  }
+  
   render() {
     return (
       <div>
